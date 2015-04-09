8201aaea9caa1a33933fc8cc7f0c1ccf253b5323
dcb9 <jt1dcb@163.com>
2015-04-09 17:14:59 +0800

    delete variables to php-fpm config

--- run.sh
+++ run.sh
@@ -2,21 +2,6 @@
 
 set -e
 
-function setEnvironmentVariable() {
-    if [ -z "$2" ]; then
-            echo "Environment variable '$1' not set."
-            return
-    fi
-    echo "env[$1] = \"$2\"" >> /etc/php5/fpm/pool.d/www.conf
-}
-
-# Grep all ENV variables
-for _curVar in `env | awk -F = '{print $1}'`;do
-    # awk has split them by the equals sign
-    # Pass the name and value to our function
-    setEnvironmentVariable ${_curVar} ${!_curVar}
-done
-
 # prepare log output
 mkdir -p /app/runtime/logs /app/web/assets
 touch /var/log/nginx/access.log \
